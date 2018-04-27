14dd6c18078cf4a166cfadd5a2a49ceb7a62d7d3
NTGAYEON <NTGAYEON@NTGAYEON-PC>
2018-04-27 19:06:44 +0900

    second commit

--- gitTest/src/gitTest/git.java
+++ gitTest/src/gitTest/git.java
@@ -4,6 +4,10 @@ public class git {
 	public static void main(String[] args) {
 		System.out.println("ge");
 	
+		
+		int a=5, b=10;
+		System.out.println("hello world");
+		System.out.println(a+b);
 	}
 
 }
